455cdd220c2c5e25d65770a4774e61bd10e8572b
ForTheOnesThere <jamescockayne1@gmail.com>
2021-01-10 11:28:59 +0000

    Makes request for token from spotify accounts service

--- server.js
+++ server.js
@@ -2,20 +2,29 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const fetch = require('node-fetch');
 
 //Start the express server
 const app = express();
-
-const code = 'abcd';
+const SECRET = process.env.SECRET
 
 //Middleware
 app.use(cors());
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}))
 
 //Endpoints
-app.get('/', (req, res) => {res.json('Server is running')})
-
+app.post('/', (req, res) => {
+    const { code, clientId, redirect } = req.body
+    fetch('https://accounts.spotify.com/api/token', {
+        method: 'POST',
+        headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+        body: `grant_type=authorization_code&code=${code}&client_id=${clientId}&redirect_uri=${redirect}&client_secret=${SECRET}`
+      })
+        .then(result => {return result.json()})
+        .then(object => res.json(object))
+})
 //Start listening
-app.listen(3001, () => {
-    console.log(`App running on port 3001`)
+app.listen(process.env.PORT, () => {
+    console.log(`App running on port ${process.env.PORT}`)
 });
